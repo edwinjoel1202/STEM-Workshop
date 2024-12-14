128a63abc7682a06d7b88681fd9f6f1f6315031d
edwinjoel1202 <edwinjoel1204@gmail.com>
2024-12-14 17:04:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,31 @@ const authRoutes = require("./routers/authRoutes");
 const moviesRoutes = require("./routers/moviesRoutes");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const { logRequest } = require("./middleware/logger");
 
 const app = express();
 
 app.use(express.json());
+app.use(logRequest);
 app.use("/", authRoutes);
 app.use("/movies", moviesRoutes);
 
+
+app.use((error, req, res, next) => {
+  try {
+    if(error instanceof SyntaxError && error.status === 400 && error.body) {
+      res.status(400).send({
+	      "status":"ERROR", "data":"Error in req body", "statusCode":401
+      });
+    }
+    else
+	    next();
+  } catch (err) {
+    res.status(500).send({
+	    "status":"ERROR", "data":"Error in server", "statusCode":500
+    });
+  }
+});
 
   mongoose
   .connect("mongodb://localhost:27017/MovieBuffet")
